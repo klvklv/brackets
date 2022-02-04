41fc9d563a35dd8b6b605e5ab7dc13c02d004553
klvklv <une_dina@mail.ru>
2022-02-04 22:53:26 +0200

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let confMap = new Map(bracketsConfig);
+	let array = str.split("");
+	if(array.length%2 != 0) return false;
+	let tempArray = [];
+	for(const bracket of array) {
+		if(bracket == confMap.get(bracket)) { // not pair (single) bracket
+			if (tempArray.length>0 && bracket == tempArray[tempArray.length - 1]) tempArray.pop();
+			else tempArray.push(bracket);
+		}
+		else {
+			if(confMap.has(bracket)) tempArray.push(bracket); // alwais push open bracket
+			else { // bracket is a close bracket
+				if(tempArray.length == 0) return false;
+				let lastCloseBracket = confMap.get(tempArray[tempArray.length - 1]);
+				if(lastCloseBracket == bracket) tempArray.pop();
+				else return false;
+			}
+		}
+	}
+	return tempArray.length == 0;
 }
